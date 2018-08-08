5418b84ca860b5c2d9129cecf1854518fb1db1da
art <art.huang@ehsn.com.tw>
2018-08-08 14:49:13 +0800

    首頁ProtoType -- LOGO區塊、新增文章按鈕、會員區塊

---
+++ MsgBoard/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MsgBoard.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        [Route("~/")]
+        [Route("Home")]
+        [Route("Post")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
