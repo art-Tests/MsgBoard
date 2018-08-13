d8e3307dc120bb305754cf50f0be25ede0016831
art <art.huang@ehsn.com.tw>
2018-08-13 09:53:45 +0800

    狀態Label改用Helper

---
+++ MsgBoard/Helper/TagHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MsgBoard.Helper
+{
+    public static class TagHelper
+    {
+        public static string CreateTag(this HtmlHelper helper, bool isEnable, string text)
+        {
+            return isEnable
+                ? $"<label class=\"label label-warning\">{text}</label>"
+                : string.Empty;
+        }
+    }
+}
