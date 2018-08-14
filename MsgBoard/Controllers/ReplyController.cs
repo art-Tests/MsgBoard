cf6413d4f380a1e5d879e93f6a145a8a61c47260
art <art.huang@ehsn.com.tw>
2018-08-14 11:16:34 +0800

    create reply ok

--- MsgBoard/Controllers/ReplyController.cs
+++ MsgBoard/Controllers/ReplyController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Net;
 using System.Web.Mvc;
+using MsgBoard.Filter;
+using MsgBoard.Models.Dto;
+using MsgBoard.Models.Entity;
 using MsgBoard.Services;
 
 namespace MsgBoard.Controllers
@@ -7,7 +10,9 @@ namespace MsgBoard.Controllers
     public class ReplyController : BaseController
     {
         private readonly PostService _postService = new PostService();
+        private readonly ReplyService _replyService = new ReplyService();
 
+        [HttpGet, AuthorizePlus]
         public ActionResult Create(int? id)
         {
             //參數錯誤
@@ -29,6 +34,17 @@ namespace MsgBoard.Controllers
 
             ViewBag.postId = id;
             return View();
+        }
+
+        [HttpPost, AuthorizePlus]
+        public ActionResult Create(Reply model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            model.CreateUserId = SignInUser.User.Id;
+            model.UpdateUserId = SignInUser.User.Id;
+            _replyService.Create(Conn, model);
+            return RedirectToAction("Index", "Post");
         }
     }
 }
---
+++ MsgBoard/Services/ReplyService.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Dapper;
+using MsgBoard.Models.Entity;
+
+namespace MsgBoard.Services
+{
+    public class ReplyService
+    {
+        public int Create(IDbConnection conn, Reply model)
+        {
+            var sqlCmd = GetCreateSqlCmd();
+            return conn.QueryFirstOrDefault<int>(sqlCmd, model);
+        }
+
+        private string GetCreateSqlCmd()
+        {
+            return @"
+INSERT INTO [dbo].[Reply]
+           ([PostId]
+           ,[Content]
+           ,[IsDel]
+           ,[CreateUserId]
+           ,[UpdateUserId])
+     VALUES
+           (@PostId
+           ,@Content
+           ,0
+           ,@CreateUserId
+           ,@UpdateUserId)
+
+SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]
+";
+        }
+    }
+}
